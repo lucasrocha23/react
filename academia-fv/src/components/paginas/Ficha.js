c1fe44befe8dd6178cc0fcea3ff9a5b78f516c6d
Lucas Rocha <lucasrochadoo123@gmail.com>
2024-02-01 14:08:32 -0300

    adição das features de salvar edição e excluir ficha na página de fichas

--- academia-fv/src/components/paginas/Ficha.js
+++ academia-fv/src/components/paginas/Ficha.js
@@ -1,14 +1,19 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from "react"
 
 import styles from './Ficha.module.css'
 import FormFicha from '../fichas/FormFicha'
+import Mensagem from '../layout/Mensagem'
 
 function Ficha(){
     const {id} = useParams()
 
+    const historico = useNavigate()
+
     const [ficha, setFicha] = useState([])
     const [editar, setEditar] = useState(false)
+    const [mensagem, setMensagem] = useState()
+    const [tipo, setTipo] = useState()
 
     useEffect(() => {
         fetch(`http://localhost:5002/fichas/${id}`, {
@@ -27,15 +32,47 @@ function Ficha(){
         setEditar(!editar) 
     }
 
-    function salvar_edicao(){
-        console.log('apertou');
+    function salvar_edicao(ficha_atualizada){
+        setMensagem('')
+
+        fetch(`http://localhost:5002/fichas/${id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify(ficha_atualizada),
+        }).then(resp => resp.json())
+        .then(data => {
+            setFicha(data)
+            setMensagem("Ficha atualizada!")
+            setTipo('sucesso')
+            editar_cancelar()
+        })
+        .catch(err => console.log(err))
+    }
+
+    function excluir(){
+        fetch(`http://localhost:5002/fichas/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            historico('/Fichas',{state: {mensagem: "Ficha excluida"}})
+        })
+        .catch(err => console.log(err))
     }
 
 
     return(
         <div className={styles.ficha_container}>
+            {mensagem && <Mensagem type={tipo} msg={mensagem}/>}
             <h2>Edite a ficha</h2>
-            <button className={styles.bt_edita} onClick={editar_cancelar}>{!editar ? "Editar" : "Cancelar"}</button>
+            <div>
+                <button className={styles.bt} onClick={editar_cancelar}>{!editar ? "Editar" : "Cancelar"}</button>
+                <button className={styles.bt} onClick={excluir}>Excluir</button>
+            </div>
             <div className={styles.ficha_info}>
                 {/* {ficha.id && <FormFicha handleSubmit={salvar_edicao} textoBt={'Salvar'} dadosFicha={ficha} disabled={!editar}/>} */}
                 {ficha.id && (editar ? (
--- academia-fv/src/components/paginas/Fichas.js
+++ academia-fv/src/components/paginas/Fichas.js
@@ -1,14 +1,21 @@
+import { useLocation } from 'react-router-dom'
+
 import { useState,useEffect } from "react"
 
 import styles from "./Fichas.module.css"
 
 import Pesquisa from "../../components/layout/Pesquisa"
 import CardFicha from "../../components/fichas/CardFicha"
+import Mensagem from '../layout/Mensagem'
 
 
 function Fichas(){
     const [fichas, setFichas] = useState([])
     const [pesquisa,setPesquisa] = useState('')
+    const [mensagem, setMensagem] = useState()
+    const [tipo, setTipo] = useState()
+
+    const location = useLocation()
 
     useEffect(() => {
         fetch(`http://localhost:5002/fichas`, {
@@ -19,10 +26,16 @@ function Fichas(){
         }).then(resp => resp.json())
         .then(data => setFichas(data))
         .catch((err) => console.log(err))
+
+        if(location.state){
+            setMensagem(location.state.mensagem)
+            setTipo('sucesso')
+        }
     }, [])
 
     return(
         <div className={styles.fichas_container}>
+            {mensagem && <Mensagem type={tipo} msg={mensagem}/>}
             <Pesquisa pesquisa={pesquisa} setPesquisa={setPesquisa}/>
 
             <div className={styles.resultados_container}>
@@ -33,7 +46,7 @@ function Fichas(){
                         a.nome.localeCompare(b.nome)
                     ).map(ficha => (
                         <div className={styles.resultados_container}>
-                            <CardFicha id={ficha.id} key={ficha.id}/>
+                            <CardFicha id={ficha.id} key={ficha.id} />
                         </div>
                 ))) : (
                     <p>Sem fichas</p>
