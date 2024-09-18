07785fc2748d7ec794587592c1b0c83cc28c549c
Lucas Rocha <lucasrochadoo123@gmail.com>
2024-09-18 16:55:18 -0300

    criação da página home no projeto emprestimo de chaves

--- emprestimo-de-chaves/src/paginas/home/index.tsx
+++ emprestimo-de-chaves/src/paginas/home/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/Cross Keys.png'
+import './estilos.css'
+
 
 function Home(){
-
     return(
-        <div>
-            <h1>home</h1>
+        <div className='container-conteudo-home'>
+            <h1>Bem Vindo ao gerenciador de chaves</h1>
+            <p>Gerencie o emprestimo e devolução de chaves de forma mais prática</p>
+            <Link className='bt-emprestimos' to={'/emprestimos'}>Realizar emprestimo/devolução</Link>
+            <img src={Logo} alt='logo'/>
         </div>
     )
 }
